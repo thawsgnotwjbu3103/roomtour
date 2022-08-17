35160a9b1edc189e10d00055d7856d263cd55907
Nong Thang <thang.nd3103@gmail.com>
2022-08-17 17:13:42 +0700

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+  <BrowserRouter>
+    <Route exact path="/">
+      <App />
+    </Route>
+    <Route exact path='/manager' component={() => {window.location.replace("http://192.168.80.6:3001/manager"); return null;}}/>
+  </BrowserRouter>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
